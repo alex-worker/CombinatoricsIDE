499e995d011aee7b9ea84f331206ec964fadbb4a
alex-worker <43813540+alex-worker@users.noreply.github.com>
2019-07-11 11:07:16 +0300

    routing working

--- simple-ide/src/App.js
+++ simple-ide/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './App.css'
 
-import Main from './Main'
+// import Main from './Main'
+
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
 import Sidenav from './components/gui/Sidenav'
 // import Link from './components/gui/Link'
+
+import About from './pages/About'
+import Cells from './pages/Cells'
+import Items from './pages/Items'
+import Messages from './pages/Messages'
+import Variables from './pages/Variables'
 
 // import M from 'materialize-css'
 // M.AutoInit()
@@ -15,15 +23,24 @@ import Sidenav from './components/gui/Sidenav'
 function App () {
   return (
     <div className='App'>
-      <Sidenav>
-        <li><a href='#vars'><i className='material-icons'>list</i>Variables</a></li>
-        <li><a href='#cells'><i className='material-icons'>grid_on</i>Cells</a></li>
-        <li><a href='#items'><i className='material-icons'>work</i>Items</a></li>
-        <li><a href='#mess'><i className='material-icons'>message</i>Messages</a></li>
-        <li><div className='divider' /></li>
-        <li><a href='#about'><i className='material-icons'>info</i>About</a></li>
-      </Sidenav>
-      <Main />
+      <Router>
+        <Sidenav>
+          <li><Link to='/vars'><i className='material-icons'>list</i>Variables</Link></li>
+          <li><Link to='/cells'><i className='material-icons'>grid_on</i>Cells</Link></li>
+          <li><Link to='/items'><i className='material-icons'>work</i>Items</Link></li>
+          <li><Link to='/mess'><i className='material-icons'>message</i>Messages</Link></li>
+          <li><div className='divider' /></li>
+          <li><Link to='/about'><i className='material-icons'>info</i>About</Link></li>
+        </Sidenav>
+        <div className='main'>
+          <Route exact path='/' component={About} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/cells' component={Cells} />
+          <Route exact path='/items' component={Items} />
+          <Route exact path='/mess' component={Messages} />
+          <Route exact path='/vars' component={Variables} />
+        </div>
+      </Router>
     </div>
   )
 }
