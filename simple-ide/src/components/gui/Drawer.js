e8fc479a565b33c42fd2f767013e379604450059
alex-worker <43813540+alex-worker@users.noreply.github.com>
2019-08-13 13:49:40 +0300

    open/close navbar

---
+++ simple-ide/src/components/gui/Drawer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+const MyDrawer = ({ open, onClose }) => {
+  return <Drawer
+    open={open}
+    onClose={onClose}
+  >
+    <Divider />
+    <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+  </Drawer>
+}
+
+export default MyDrawer
--- simple-ide/src/components/gui/Menu.js
+++ simple-ide/src/components/gui/Menu.js
@@ -1,17 +1,55 @@
 import React from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
-const Menu = () => {
-  return <AppBar position='static'>
-    <Toolbar>
-      <IconButton edge='start' color='inherit' aria-label='menu'>
-        <MenuIcon />
-      </IconButton>
-    </Toolbar>
-  </AppBar>
+import Drawer from './Drawer'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  header: {
+    flexGrow: 1
+  }
+}))
+
+const Menu = ({ header = 'undefined' }) => {
+  const classes = useStyles()
+
+  const [isDrawerOpen, setDrawerOpen] = React.useState(false)
+
+  const openDrawer = () => {
+    setDrawerOpen(true)
+  }
+
+  const closeDrawer = () => {
+    setDrawerOpen(false)
+  }
+
+  return <div className={classes.root}>
+    <AppBar position='static'>
+      <Toolbar>
+        <IconButton color='inherit' className={classes.menuButton} onClick={openDrawer} >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant='h6' className={classes.header} >
+          {header}
+        </Typography>
+        <Button color='inherit' >Login</Button>
+      </Toolbar>
+    </AppBar>
+    <Drawer open={isDrawerOpen} onClose={closeDrawer} />
+  </div>
 }
 
 export default Menu
