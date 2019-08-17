d69fc296f9175c85b0d8cccf47dba964311c343f
alex-worker <43813540+alex-worker@users.noreply.github.com>
2019-08-17 20:44:12 +0300

    try begin to use react-window and material-ui

--- simple-ide/src/pages/Variables.js
+++ simple-ide/src/pages/Variables.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { FixedSizeList as List } from 'react-window'
+import InfiniteLoader from 'react-window-infinite-loader'
+// import AutoSizer from 'react-virtualized-auto-sizer'
+
+// ok try this https://medium.com/@joedister/using-react-virtualized-infiniteloader-autosizer-and-table-with-material-ui-styles-react-76d3596b6c93
 
 const vars = [
   { name: 'var_act', default: '0', comment: 'Current action' },
@@ -29,9 +34,32 @@ const columns = [
   }
 ]
 
+const MyRow = () => {
+  return <div>'Row'</div>
+}
+
+const MyInfiniteList = () => {
+  return <InfiniteLoader>
+    {({ onItemsRendered, ref }) => {
+      return <List
+        className="List"
+        height={150}
+        itemCount={1000}
+        itemSize={30}
+        onItemsRendered={onItemsRendered}
+        ref={ref}
+        width={300}
+      >
+        {MyRow}
+      </List>
+    }}
+  </InfiniteLoader>
+}
+
 const Variables = () => {
-  return <div>
+  return <div >
     <h1>'Variables'</h1>
+    <MyInfiniteList />
   </div>
 }
 
