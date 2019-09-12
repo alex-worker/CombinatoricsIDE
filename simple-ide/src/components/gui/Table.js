fe2eb931dafad5b9e71c9d524d05f5d982ffa66f
AlexWorker <AlexWorker@protonmail.com>
2019-09-12 10:42:36 +0300

    работает обычный map без всяких virtualize, может так проще

--- simple-ide/src/components/gui/Table.js
+++ simple-ide/src/components/gui/Table.js
@@ -1,23 +1,25 @@
 import React from 'react'
 
+import { map as ObjectMap } from '../../utils/map'
+
 const _showHeader = (columns) => {
   return columns.map((item, index) => {
     return <td key={index}>{item.label}</td>
   })
 }
 
-const _showRow = (item) => {
-  return item.map((item, index) => {
-    return <td>
-      lol
+const _showRow = (item, index) => {
+  return (
+    <td key={index}>
+      {item}
     </td>
-  })
+  )
 }
 
 const _showRows = (list) => {
   return list.map((item, index) => (
     <tr key={index}>
-      {_showRow(item)}
+      {ObjectMap(item, _showRow)}
     </tr>
   ))
 }
---
+++ simple-ide/src/utils/map.js
@@ -0,0 +1,19 @@
+// кажется это не нужно вообще
+exports.checkProperty = (key, obj) => {
+  if (!(key in obj)) {
+    return false
+  }
+  return true
+}
+
+exports.map = (obj, func) => {
+  const ret = []
+  let index = 0
+  for (var key in obj) {
+    if (key in obj) { // not use hasOwnProperty а то ругается линтер
+      ret.push(func(obj[key], index))
+      index++
+    }
+  }
+  return ret
+}
