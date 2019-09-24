6d00b56a983f42d86834f9d5f8185b884fe42b92
AlexWorker <AlexWorker@protonmail.com>
2019-09-24 11:32:56 +0300

    me little storage

--- simple-ide/src/storage/Storage.js
+++ simple-ide/src/storage/Storage.js
@@ -2,14 +2,16 @@ import PouchDB from 'pouchdb'
 import pouchdbDebug from 'pouchdb-debug'
 
 const MyPouchDb = PouchDB.plugin(pouchdbDebug)
-MyPouchDb.debug.enable('*')
+MyPouchDb.debug.enable('pouchdb:*') // если указать просто '*' начинается жесткий флуд в консоль из-за вебсокетов реакта
 
 class Storage {
+  // db - PouchDB connector
   constructor () {
     this.db = new MyPouchDb('simple-ide')
-    this.db.info().then(function (info) {
-      console.log(info)
-    })
+    // this.db.info().then(info => {
+    //   console.log('constructor!!!')
+    //   this.info = info
+    // })
   }
 }
 
