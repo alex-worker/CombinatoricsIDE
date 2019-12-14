ea02ca13766b3dc48aad1c15406acccc9192a436
AlexWorker <AlexWorker@protonmail.com>
2019-12-15 00:12:59 +0300

    fix

--- simple-ide/src/components/gui/List/index.js
+++ simple-ide/src/components/gui/List/index.js
@@ -5,47 +5,6 @@ import './gui.css'
 
 import Scroll from './Scroll'
 
-// let sizeMap = []
-
-// const Row = ({ width, index, data }) => {
-//   const root = React.useRef()
-
-//   const setSize = React.useCallback((index, size) => {
-//     console.log(index, size)
-//     sizeMap = { ...sizeMap, [index]: size }
-//   }, [])
-
-//   React.useEffect(() => {
-//     const height = root.current.getBoundingClientRect().height
-//     setSize(index, height)
-//     // console.log(height)
-//   }, [width])
-
-//   return (
-//     <div ref={root} className='list-item'>
-//       {index}: {data.name} {data.description}
-//     </div>
-//   )
-// }
-
-// const sizeMap = React.useRef({})
-// const setSize = React.useCallback((index, size) => {
-//   sizeMap.current = { ...sizeMap.current, [index]: size }
-// }, [])
-// const getSize = React.useCallback(index => sizeMap.current[index] || 50, [])
-
-// const Row = ({ width, index, data }) => {
-//   const root = React.useRef()
-//   React.useEffect(() => {
-//     setSize(index, root.current.getBoundingClientRect().height)
-//   }, [])
-//   return (
-//     <div ref={root} className='list-item'>
-//       {index}: {data.name} {data.description}
-//     </div>
-//   )
-// }
-
 const MyList = ({ list }) => {
   const [width, height] = useWindowSize()
   return (
