263194c6b93d65b7ecb83b7e0a20e621a0cd9a5c
AlexWorker <AlexWorker@protonmail.com>
2020-02-04 14:36:09 +0300

    yarn update

--- gbased-ide/src/components/App.js
+++ gbased-ide/src/components/App.js
@@ -4,10 +4,30 @@ import AppToolbar from '../containers/AppToolbar'
 
 import '../styles/App.css'
 
+const l10n = (str) => {
+  return str
+}
+
+const sectionNames = {
+  world: l10n('NAV_GAME_WORLD'),
+  sprites: l10n('NAV_SPRITES'),
+  backgrounds: l10n('NAV_BACKGROUNDS'),
+  ui: l10n('NAV_UI_ELEMENTS'),
+  music: l10n('NAV_MUSIC'),
+  dialogue: l10n('NAV_DIALOGUE_REVIEW'),
+  build: l10n('NAV_BUILD_AND_RUN'),
+  settings: l10n('NAV_SETTINGS')
+}
+
 function App () {
+  const [section, setSection] = React.useState('world')
+  const onSectionChange = (key) => {
+    setSection(key)
+  }
+  console.log('App!')
   return (
     <div className='App'>
-      <AppToolbar />
+      <AppToolbar sectionNames={sectionNames} curSection={section} onSectionChange={onSectionChange} />
     </div>
   )
 }
--- gbased-ide/src/containers/AppToolbar.js
+++ gbased-ide/src/containers/AppToolbar.js
@@ -9,39 +9,38 @@ import {
 
 import { MenuItem } from '../components/library/Menu'
 
-const l10n = (str) => {
-  return str
-}
+// const l10n = (str) => {
+//   return str
+// }
 
 const name = 'Combinatorica IDE'
-// const section = 'world'
 
-const sectionNames = {
-  world: l10n('NAV_GAME_WORLD'),
-  sprites: l10n('NAV_SPRITES'),
-  backgrounds: l10n('NAV_BACKGROUNDS'),
-  ui: l10n('NAV_UI_ELEMENTS'),
-  music: l10n('NAV_MUSIC'),
-  dialogue: l10n('NAV_DIALOGUE_REVIEW'),
-  build: l10n('NAV_BUILD_AND_RUN'),
-  settings: l10n('NAV_SETTINGS')
-}
+// const sectionNames = {
+//   world: l10n('NAV_GAME_WORLD'),
+//   sprites: l10n('NAV_SPRITES'),
+//   backgrounds: l10n('NAV_BACKGROUNDS'),
+//   ui: l10n('NAV_UI_ELEMENTS'),
+//   music: l10n('NAV_MUSIC'),
+//   dialogue: l10n('NAV_DIALOGUE_REVIEW'),
+//   build: l10n('NAV_BUILD_AND_RUN'),
+//   settings: l10n('NAV_SETTINGS')
+// }
 
-const AppToolbar = () => {
-  const [section, setSection] = React.useState('world')
+const AppToolbar = ({ sectionNames, headerName = 'UNDEFINED', curSection, onSectionChange }) => {
+  // const [section, setSection] = React.useState(curPage)
   return (
     <Toolbar>
       <Helmet>
         <title>{name}</title>
       </Helmet>
       <ToolbarDropdownButton
-        label={<div style={{ minWidth: 106 }}>{sectionNames[section]}</div>}
+        label={<div style={{ minWidth: 106 }}>{sectionNames[curSection]}</div>}
       >
         {Object.keys(sectionNames).map(key => (
           <MenuItem
             key={key}
             value={key}
-            onClick={() => setSection(key)}
+            onClick={() => onSectionChange(key)}
             style={{ minWidth: 200 }}
           >
             {sectionNames[key]}
