06367bb9a7166ca0b01d7ae4a4d0540fee698e3e
AlexWorker <AlexWorker@protonmail.com>
2020-02-04 14:15:17 +0300

    set correct nav

--- gbased-ide/src/containers/AppToolbar.js
+++ gbased-ide/src/containers/AppToolbar.js
@@ -3,17 +3,51 @@ import { Helmet } from 'react-helmet'
 
 import {
   Toolbar,
-  ToolbarTitle
+  ToolbarTitle,
+  ToolbarDropdownButton
 } from '../components/library/Toolbar'
 
+import { MenuItem } from '../components/library/Menu'
+
+const l10n = (str) => {
+  return str
+}
+
 const name = 'Combinatorica IDE'
+// const section = 'world'
+
+const sectionNames = {
+  world: l10n('NAV_GAME_WORLD'),
+  sprites: l10n('NAV_SPRITES'),
+  backgrounds: l10n('NAV_BACKGROUNDS'),
+  ui: l10n('NAV_UI_ELEMENTS'),
+  music: l10n('NAV_MUSIC'),
+  dialogue: l10n('NAV_DIALOGUE_REVIEW'),
+  build: l10n('NAV_BUILD_AND_RUN'),
+  settings: l10n('NAV_SETTINGS')
+}
 
 const AppToolbar = () => {
+  const [section, setSection] = React.useState('world')
   return (
     <Toolbar>
       <Helmet>
         <title>{name}</title>
       </Helmet>
+      <ToolbarDropdownButton
+        label={<div style={{ minWidth: 106 }}>{sectionNames[section]}</div>}
+      >
+        {Object.keys(sectionNames).map(key => (
+          <MenuItem
+            key={key}
+            value={key}
+            onClick={() => setSection(key)}
+            style={{ minWidth: 200 }}
+          >
+            {sectionNames[key]}
+          </MenuItem>
+        ))}
+      </ToolbarDropdownButton>
       <ToolbarTitle>
         {name || 'Untitled'}
       </ToolbarTitle>
